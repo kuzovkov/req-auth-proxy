0ecab051b2f1b6018391f9c75b3edf53bf8e8362
Your Name <you@example.com>
2016-11-23 15:30:48 +0300

    next

--- tp.py
+++ tp.py
@@ -9,14 +9,17 @@ proxy_address = '62.109.2.4:3129'
 proxy_account = 'Buffa4ok:lAs5ok7y'
 use_proxy = True
 
-
-try:
+def add_proxy():
     if use_proxy:    
         proxy_handler = urllib2.ProxyHandler({'https': ''.join(['https://',proxy_account,'@',proxy_address])})
         proxy_auth_handler = urllib2.ProxyBasicAuthHandler()
         opener = urllib2.build_opener(proxy_handler, proxy_auth_handler, urllib2.HTTPHandler)
         urllib2.install_opener(opener)
-    res = urllib2.urlopen(url)
+
+try:
+    req = urllib2.Request(url)    
+    add_proxy()
+    res = urllib2.urlopen(req)
 except Exception, ex:
     print "Не могу открыть ",url
     print ex
@@ -27,3 +30,5 @@ else:
     if need_content is not None:
         print need_content.group()
     print "-"*60
+    
+    
